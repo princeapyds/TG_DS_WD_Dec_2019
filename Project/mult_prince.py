eeb733024477a4263552f6c399bbe47a5a132e94
Computer <tejak.2294@gmail.com>
2019-12-16 10:59:48 +0530

    113

--- Project/mult_prince.py
+++ Project/mult_prince.py
@@ -1,8 +1,5 @@
-def mult_prince(num1,num2):
-        print(num1*num2)
+def mult_prince(num1,num2,num3):
+        print(num1*num2*num3)
 
 
-mult_prince(10,20)
-mult_prince(10,20)
-mult_prince(10,20)
-
+mult_prince(10,20,30)
