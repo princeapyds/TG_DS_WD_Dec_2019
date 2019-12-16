c0684613e4c41cd4a1284bb79ede0de523795c8f
MyPC <shw83>
2019-12-16 11:14:42 +0530

    CR-114_shweta

--- Project/sum_niraj.py
+++ Project/sum_niraj.py
@@ -1,4 +1,4 @@
-def add_nums(num1,num2):
-    num3 = num1 + num2
-    print ("Sum of twi numbers are:",num3)
-add_nums(5,6)
+def add_nums(num1,num2,num3):
+    num4 = num1 + num2 + num3
+    print ("Sum of three numbers are:",num4)
+add_nums(5,6,7)
